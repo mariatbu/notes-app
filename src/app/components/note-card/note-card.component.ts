f51085d48b587351f591fc2b1df3f7300b035044
María Turpín <maria.turpin.buendia98@gmail.com>
2021-11-08 17:50:43 +0100

    Created basic pages with style

---
+++ src/app/components/note-card/note-card.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-note-card',
+  templateUrl: './note-card.component.html',
+  styleUrls: ['./note-card.component.scss']
+})
+export class NoteCardComponent implements OnInit {
+
+  @ViewChild('truncator', {static: true}) truncator!: ElementRef<HTMLElement>;
+  @ViewChild('bodyText', {static:true}) bodyText!: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+
+    if(this.bodyText.nativeElement.scrollHeight > viewableHeight){
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    }else{
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
+  }
+
+}
